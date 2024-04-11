05e5cf79d4b8c89e500c3f6fcfe5e651a0cf8603
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-04-11 12:03:37 -0300

    casi terminado

--- index.js
+++ index.js
@@ -17,34 +17,40 @@ app.get('/', (req, res) => {
 })
 
 app.get('/saludar/:nombre', (req, res) => {
-    res.status(200).send('Hola ' + req.params.nombre + ' (200)')
+    res.status(200).send('Hola ' + req.params.nombre)
 })
 
 app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
-    if (date.getFullYear() == year && date.getMonth() == month && date.getDate() == day){
+    let fecha = req.params.ano + '-' + req.params.mes + '-' + req.params.dia
+    let numFecha = Date.parse(date)
+    if (numFecha != null){
         res.status(200).send('OK (200)')
     } else{
-        res.status(400).send('Error(400)')
+        res.status(400).send('Error (404)')
     }
 })
 
-app.get('/matematica/sumar?n1={5}&n2={9}', (req, res) => {
-    res.status(200).send('El resultado es ' + req.params.sumar + ' (200)') 
+app.get('/matematica/sumar?n1={numero}&n2={numero}', (req, res) => {
+    let resultado = req.params.n1 + req.params.n2
+    res.status(200).send('El resultado es ' + resultado + ' (200)') 
 })
 
-app.get('/matematica/restar?n1={5}&n2={9}', (req, res) => {
-    res.status(200).send('El resultado es ' + req.params.restar + ' (200)') 
+app.get('/matematica/restar?n1={numero}&n2={numero}', (req, res) => {
+    let resultado = req.params.n1 - req.params.n2
+    res.status(200).send('El resultado es ' + resultado + ' (200)') 
 })
 
-app.get('/matematica/multiplicar?n1={5}&n2={9}', (req, res) => {
-    res.status(200).send('El resultado es ' + req.params.multiplicar + ' (200)') 
+app.get('/matematica/multiplicar?n1={numero}&n2={numero}', (req, res) => {
+    let resultado = req.params.n1 * req.params.n2
+    res.status(200).send('El resultado es ' + resultado + ' (200)') 
 })
 
-app.get('/matematica/dividir?n1={5}&n2={9}', (req, res) => {
+app.get('/matematica/dividir?n1={numero}&n2={numero}', (req, res) => {
+    let resultado = req.params.n1 / req.params.n2
     if(req.params.n2 != 0){
-        res.status(200).send('El resultado es ' + req.params.dividir + ' (200)') 
+        res.status(200).send('El resultado es ' + resultado + ' (200)') 
     } else{
-        res.status(400).send('El divisor no puede ser 0 (400)')
+        res.status(400).send('El divisor no puede ser 0 (404)')
     }
 })
 
@@ -52,11 +58,40 @@ app.get('/omdb/searchbypage?search={texto}&p={pagina}', (req, res) => {
     res.status(200).send(req.params.texto + req.params.pagina + ' (200)')
 })
 
-app.get('"/omdb/searchcomplete?search={texto}', (req, res) => {
+app.get('/omdb/searchcomplete?search={texto}', (req, res) => {
     res.status(200).send(req.params.texto + ' (200)')
 })
 
+app.get('/omdb/getbyomdbid?imdbID={imdb}', (req, res) => {
+    res.status(200).send(req.params.imdb + ' (200)')
+})
 
+const alumnosArray = [];
+alumnosArray.push(new Alumno("Esteban Dido" , "22888444", 20));
+alumnosArray.push(new Alumno("Matias Queroso", "28946255", 51));
+alumnosArray.push(new Alumno("Elba Calao" , "32623391", 18));
+
+app.get('/alumnos', (req, res) => {
+    res.status(200).send(req.params.alumnos + ' (200)')
+})
+
+app.get('/alumnos/:dni', (req, res) => {
+    let resultado = alumnosArray.find(req.params.dni)
+    res.status(200).send(resultado + ' (200)')
+})
+
+app.post('/alumnos', (req, res) => {
+    let resultado = alumnosArray.push(req.body)
+    res.status(201).send(resultado + ' (201)')
+})
+
+app.delete('/alumnos', (req, res) => {
+    if(alumnosArray.find(req.body.dni)){
+        res.status(200).send('OK (200)')
+    } else{
+        res.status(400).send('No se encontro (404)')
+    }
+})
 
 
 
